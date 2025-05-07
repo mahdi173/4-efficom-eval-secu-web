1412ef71ddecd3d8cd5827ecc4561ad475718ae4
mahdi173 <zougarimehdi44@gmail.com>
2025-05-07 10:55:25 +0200

    correction role_route: exposer les routes de role au public

--- route/role.route.js
+++ route/role.route.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const roleController = require('../controller/role.controller.js');
 const auth = require('../middleware/auth.middleware.js');
 
-router.get('/', roleController.getAll);
-router.get('/:id', roleController.getById);
+router.get('/',  auth("Admin"),roleController.getAll);
+router.get('/:id',  auth("Admin"), roleController.getById);
 
-router.post('/',auth, roleController.create);
+router.post('/', auth("Admin"), roleController.create);
 
-router.put('/:id',auth, roleController.update);
-router.delete('/:id',auth, roleController.remove);
+router.put('/:id', auth("Admin"), roleController.update);
+router.delete('/:id', auth("Admin"), roleController.remove);
 
 
 
