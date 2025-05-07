3ead08051e53413220851d9ee0d4a0c2a6d42ea1
mahdi173 <zougarimehdi44@gmail.com>
2025-05-07 10:54:04 +0200

    correction auth_middleware: exposition de clé jwt

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
 const Role = require('../model/role.schema.js');
+require('dotenv').config();
 
 const auth = (role) => {
     return (req, res, next) => {
         const token = req.headers.authorization?.split(" ")[1];
         try {
-            req.payload = jwt.verify(token, "ZXZhbCBzZWN1IHdlYg==");
+            req.payload = jwt.verify(token, process.env.JWT_KEY);
             const roleToCheck = Role.getByName(role);
             if(role && req.payload.roles && !req.payload.roles.includes(roleToCheck.id)){
                 return res.status(403).json({message: "Vous n'avez pas les droits pour réaliser cette action"});
